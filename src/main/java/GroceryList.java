002a53f02362f7fc93aa40d2beaf9ac60460ca94
nathan <nathan.guardado@yahoo.com>
2023-12-11 15:54:45 -0500

    Added Error

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -7,12 +7,16 @@ public class GroceryList extends ArrayList<ArrayList<GroceryItem>> {
     ArrayList<GroceryItem> Bread;
     ArrayList<GroceryItem> Cookies;
     ArrayList<GroceryItem> Apples;
+    int Error;
 
     public GroceryList() {
         this.Milk = new ArrayList<>();
         this.Bread = new ArrayList<>();
         this.Cookies = new ArrayList<>();
         this.Apples = new ArrayList<>();
+    }
+    public int Error(){
+        return 0;
     }
 
     public ArrayList<GroceryItem> getMilk() {return Milk;}
@@ -23,5 +27,8 @@ public class GroceryList extends ArrayList<ArrayList<GroceryItem>> {
     public void setCookies(ArrayList<GroceryItem> cookies) {Cookies = cookies;}
     public ArrayList<GroceryItem> getApples() {return Apples;}
     public void setApples(ArrayList<GroceryItem> apples) {Apples = apples;}
+
+    public int getError() {return Error;}
+    public void setError(int error) {this.Error = error;}
 }
 
