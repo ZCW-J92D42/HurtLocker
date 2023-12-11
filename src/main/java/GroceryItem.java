5db14af603a84ef5b6c608317058764daaadab12
Tevin Glover <tevwork.tg@gmail.com>
2023-12-11 14:55:17 -0500

    holes in my plans

--- src/main/java/GroceryItem.java
+++ src/main/java/GroceryItem.java
@@ -1,2 +1,33 @@
 public class GroceryItem {
+    String name;
+    String price;
+    String type;
+    String expiration;
+    public GroceryItem(String name, String price, String type, String expiration) {
+        this.name = name;
+        this.price = price;
+        this.type = type;
+        this.expiration = expiration;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getExpiration() {
+        return expiration;
+    }
 }
